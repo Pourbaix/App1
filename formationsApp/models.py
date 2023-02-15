87b5c112f1a60e55300c3d277245b6f87b168358
Pourbaix <mi.pourbaix@students.ephec.be>
2023-02-15 11:21:15 +0100

    [INIT] Initializing the Django project + creating models

---
+++ formationsApp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    second_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    role = "formateur"
+
+class Formations(models.Model):
+    trainer = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+class Session(models.Model):
+    formation = models.ForeignKey(Formations, on_delete=models.CASCADE)
+    students = models.ManyToManyField(User)
+    event_date = models.DateTimeField('date of the session')
+    max_student_nbr = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10000)])
+    place = models.CharField(max_length=50)
